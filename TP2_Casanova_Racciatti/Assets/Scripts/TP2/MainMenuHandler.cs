ca777b7a9b2f3d994cd9a6cecb31faf041db7b04
TomasRacciatti <tomasracciatti@gmail.com>
2025-06-19 19:59:29 -0300

    Main Menu handler

---
+++ TP2_Casanova_Racciatti/Assets/Scripts/TP2/MainMenuHandler.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    [SerializeField] private NetworkRunnerHandler _runnerHandler;
+    
+    [Header("Panels")]
+    [SerializeField] private GameObject _mainMenuPanel;
+    [SerializeField] private GameObject _connectingPanel;
+    [SerializeField] private GameObject _browserPanel;
+    [SerializeField] private GameObject _hostPanel;
+    
+    [Header("Buttons")]
+    [SerializeField] private Button _connectButton;
+    [SerializeField] private Button _goToHostButton;
+    [SerializeField] private Button _hostButton;
+    
+    [Header("Texts")]
+    [SerializeField] private TMP_Text _connectingText;
+    
+    [Header("InputFields")] 
+    [SerializeField] private TMP_InputField _sessionNameField;
+
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+    
+}
