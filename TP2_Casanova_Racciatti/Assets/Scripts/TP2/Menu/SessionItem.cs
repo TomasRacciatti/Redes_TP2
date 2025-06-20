d40a697b94f90324aa81046803b75b0adf3e999b
TomasRacciatti <tomasracciatti@gmail.com>
2025-06-20 12:27:06 -0300

    Session item

--- TP2_Casanova_Racciatti/Assets/Scripts/TP2/Menu/SessionItem.cs
+++ TP2_Casanova_Racciatti/Assets/Scripts/TP2/Menu/SessionItem.cs
@@ -2,14 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Fusion;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SessionItem : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _sessionName;
+    [SerializeField] private TMP_Text _playerCount;
+    [SerializeField] private Button _joinButton;
+
+
     public event Action<SessionInfo> OnJoinSession;
-    
+
     public void Initialize(SessionInfo sessionInfo)
     {
-        
+        _sessionName.text = sessionInfo.Name;
+        _playerCount.text = $"{sessionInfo.PlayerCount}/{sessionInfo.MaxPlayers}";
+
+        if (sessionInfo.PlayerCount >= sessionInfo.MaxPlayers)
+        {
+            _joinButton.interactable = false;
+            return;
+        }
+
+        _joinButton.onClick.AddListener(() => { OnJoinSession?.Invoke(sessionInfo); });
     }
 }
