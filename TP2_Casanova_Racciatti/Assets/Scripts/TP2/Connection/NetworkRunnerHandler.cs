34ea3ceb236d4883282028fabb9e360c229771ce
TomasRacciatti <tomasracciatti@gmail.com>
2025-06-20 13:29:12 -0300

    Cambie la cantidad maxima de la sesion

--- TP2_Casanova_Racciatti/Assets/Scripts/TP2/Connection/NetworkRunnerHandler.cs
+++ TP2_Casanova_Racciatti/Assets/Scripts/TP2/Connection/NetworkRunnerHandler.cs
@@ -12,23 +12,18 @@ public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
     private NetworkRunner _currentRunner;
 
     public event Action OnLobbyJoined;
-    public event Action<List<SessionInfo>> OnSessionListUpdate; 
-
-    /*
-    private void Start()
-    {
-        JoinLobby();
-    }
-    */
+    public event Action<List<SessionInfo>> OnSessionListUpdate;
     
+
     public void JoinLobby()
     {
         if (_currentRunner)
             Destroy(_currentRunner.gameObject);
-        
+
         _currentRunner = Instantiate(_runnerPrefab);
-        
-        _currentRunner.AddCallbacks(this); // Necesario porque este script no va a estar en el mismo objeto que el NetworkRunner
+
+        _currentRunner
+            .AddCallbacks(this); // Necesario porque este script no va a estar en el mismo objeto que el NetworkRunner
 
         JoinLobbyAsync();
     }
@@ -42,22 +37,22 @@ public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
             Debug.LogError($"[Custom Error] Unable to join Lobby");
             return;
         }
-        
+
         Debug.Log($"[Custom Msg] Joined Lobby");
         OnLobbyJoined?.Invoke();
     }
-    
+
     public async void CreateGame(string sessionName, string sceneName)
     {
-        await InitializeGame(GameMode.Host, sessionName, SceneUtility.GetBuildIndexByScenePath($"Scenes/{sceneName}"));
+        await InitializeGame(GameMode.Host, sessionName, SceneUtility.GetBuildIndexByScenePath($"Scenes/{sceneName}"), 5);
     }
-    
+
     public async void JoinGame(SessionInfo sessionInfo)
     {
         await InitializeGame(GameMode.Client, sessionInfo.Name);
     }
-    
-    async Task InitializeGame(GameMode gameMode, string sessionName, int sceneIndex = 0)
+
+    async Task InitializeGame(GameMode gameMode, string sessionName, int sceneIndex = 0, int maxPlayers = 5)
     {
         _currentRunner.ProvideInput = true;
 
@@ -65,48 +60,30 @@ public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
         {
             GameMode = gameMode,
             Scene = SceneRef.FromIndex(sceneIndex),
-            SessionName = sessionName
+            SessionName = sessionName,
+            PlayerCount = maxPlayers,
         });
-        
+
         if (!result.Ok)
         {
             Debug.LogError($"[Custom Error] Unable to Start Game");
             return;
         }
-        
+
         Debug.Log($"[Custom Msg] Game Started");
     }
-    
+
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
     {
         OnSessionListUpdate?.Invoke(sessionList);
-        
-        /*
-        //Si no hay ninguna sala, creo
-        if (sessionList.Count == 0)
-        {
-            CreateGame("Room 0", "Game");
-        }
-        else // Me uno
-        {
-            foreach (var sessionInfo in sessionList)
-            {
-                if (sessionInfo.PlayerCount >= sessionInfo.MaxPlayers) continue;
-                
-                JoinGame(sessionInfo);
-                return;
-            }
-        }
-        */
     }
-    
+
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
     public void OnInput(NetworkRunner runner, NetworkInput input) { }
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
     public void OnConnectedToServer(NetworkRunner runner) { }
-
     public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
     public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
@@ -115,8 +92,8 @@ public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
     public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
     public void OnSceneLoadDone(NetworkRunner runner) { }
     public void OnSceneLoadStart(NetworkRunner runner) { }
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player){ }
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player){ }
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data){ }
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress){ }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
 }
